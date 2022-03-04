f7b3ad01a788ab5a439d9d4de236cc4ca9c136e0
steeltx <realsteel.gnu@gmail.com>
2022-03-03 23:06:59 -0600

    Inicio de pruebas

---
+++ src/test/demo.test.js
@@ -0,0 +1,19 @@
+// Agrupador de pruebas
+describe('Pruebas en el archivo demo.test.js', () => {
+
+    test(' deben de ser iguales los string ', () => {
+  
+        // 1. Inicialización
+       const mensaje = 'Hola Mundo';
+        
+        // 2. Estimulo
+        const mensaje2 = `Hola Mundo`;
+    
+        // 3. Observar el comportamiento
+        expect(mensaje).toBe(mensaje2);
+    
+    })
+
+});
+
+
